22277680e071b0124d3fa6b90c4a20bf37d232bd
Vanessa <vsochat@stanford.edu>
2014-11-09 13:59:03 -0800

    modified:   brainbehavior/imageutils.py 	modified:   extractRegionalFeatures.py

--- brainbehavior/imageutils.py
+++ brainbehavior/imageutils.py
@@ -16,19 +16,80 @@ SAMPLE USAGE: Please see README included with package
 
 """
 
+import os
 import nibabel
 import glob
 import numpy as np
 import pandas as pd
 from scipy import ndimage
 from nilearn.image import resample_img
-import os
+from nilearn.masking import apply_mask
 
 __author__ = ["Vanessa Sochat (vsochat@stanford.edu)","Matthew Sacchet (msacchet@stanford.edu)"]
 __version__ = "$Revision: 1.0 $"
 __date__ = "$Date: 2011/09/09 $"
 __license__ = "Python"
 
+
+
+"""Resamples image voxels to different voxel size, should be a tuple with three values
+returns a nibabel.nifti1.Nifti1Image"""
+def resize_image(input_image,voxel_dimension):
+  if len(voxel_dimension) != 3:
+    print "Please specify a list of three voxel sizes, e.g. (3,3,3)"
+    return
+
+  # If we have a filename, we need to load it
+  if isinstance(input_image,str):
+    header = nibabel.load(input_image)
+  # Or we can take a nibabel nifti1 image
+  elif isinstance(input_image,nibabel.nifti1.Nifti1Image):
+    header = input_image
+  else:
+    print "Please provide an input_image filepath OR nibabel.nifti1.Nifti1Image object!"
+    return
+  voxel_dimension = tuple(voxel_dimension)
+  target_affine = np.diag(voxel_dimension)
+  resampled = resample_img(header, target_affine=target_affine)
+
+
+"""A wrapper for spatial_normalize_image - returns a pandas dataframe of image voxels normalized (resampled) to another (assuming standard) space"""
+def spatial_normalize_images(input_images,standard):
+  image_matrix = []
+  for mr in input_images:
+    transformed_image = spatial_normalize_image(mr,standard)
+    image_matrix.append(transformed_image["data"])
+  df = pd.DataFrame(image_matrix,columns=input_images)
+  return df
+
+"""Transform a single image into another space (likely a standard we are assuming both in MNI space!)"""
+def spatial_normalize_image(image,standard):
+   standard = read_image(standard)
+   image = read_image(image)
+   header = resample_img(image["header"], target_affine=standard["affine"],target_shape=standard["shape"])
+   transformed = header.get_data()
+   return {"data":transformed,"mask":transformed > 1e-13,"shape":header.shape[:3],"affine":header.get_affine(),"file":image,"header":header}
+
+"""Returns an image in the Zscore space, thresholded at threshold"""
+def normalize_image_zscore(header,threshold=None):
+  data = header.get_data()
+  Z = (data - np.mean(data)) / np.std(data)
+  if threshold:
+    mask = Z < float(threshold)
+    Z[mask] = float(0)
+    # Return the new header object
+  return nibabel.Nifti1Image(Z,header.get_affine())
+
+"""Reads in image, returns image values and mask"""
+def read_image(image_file):
+    print "Loading image %s" %(image_file)
+    mr = nibabel.load(image_file)
+    affine = mr.get_affine()
+    shape = mr.shape[:3]
+    data = mr.get_data()
+    mask = data > 1e-13
+    mr = {"data":data,"mask":mask,"shape":shape,"affine":affine,"file":image_file,"header":mr}
+    return mr
 
 # Atlas Summary Object -------------------------------------------------------------------
 """Atlas - internal object for storing atlas labels and regions, for use to summarize the 
@@ -41,10 +102,7 @@ class Atlas:
   """Read atlas images into nifti objects - only keep those in MNI 152 space"""
   def read_standard(self,standard):
     print "Loading standard space..."
-    self.standard = nibabel.load(standard)
-    affine = self.standard.get_affine()
-    shape = self.standard.shape[:3]
-    self.mask = self.standard.get_data() > 1e-13
+    self.standard = read_image(standard)
 
   """Read atlas images into nifti objects - only keep those in MNI 152 space
   Atlas lookup should be a tab separated file, with each line having the image name,
@@ -57,13 +115,12 @@ class Atlas:
     for image in unique_images:
       print "Reading atlas image %s" % (image)
       full_path = os.path.abspath("%s/%s" % (atlas_directory,image))
-      header = nibabel.load(full_path)
-      header = resample_img(header, target_affine=self.standard.get_affine(),target_shape=self.standard.shape[:3])
-      img = header.get_data()
+      img = spatial_normalize_image(full_path,self.standard["file"])
       self.atlas_data[image] = img
 
-  """Get voxel count feature vector for one or more images against all of atlas labels"""
-  def count_voxels_in_atlas(self,mrs,output_file):
+  """Get voxel count feature vector for one or more images against all of atlas labels
+If normalize_threshold is specified, will convert image to Z score, and threshold at value"""
+  def count_voxels_in_atlas(self,mrs,output_file,normalize_threshold=False):
     feature_vectors = []
     for mr in mrs:
       print "MR image is %s" %(mr)
@@ -73,7 +130,10 @@ class Atlas:
       header = nibabel.load(full_path)
       # Only do for single timepoint images for now
       if len(header.shape) == 3:
-        header = resample_img(header, target_affine=self.standard.get_affine(),target_shape=self.standard.shape[:3])
+        header = resample_img(header, target_affine=self.standard["affine"],target_shape=self.standard["shape"])
+        # If the user hasn't specified a normalize_threshold, just take image data as is'
+        if normalize_threshold:
+          header = normalize_image_zscore(header,normalize_threshold)
         img = header.get_data()
         # For each label in the atlas, get a voxel count
         for atlas_image,image_data in self.atlas_data.iteritems():
@@ -83,8 +143,10 @@ class Atlas:
             voxel_value = row[1]["label"]
             region_label = row[1]["regionName"]
             feature_labels.append("%s_%s" % ( atlas_image, region_label ))
-            region_mask = image_data == voxel_value
-            region_mask = np.logical_and(region_mask, self.mask)
+            # Create the region mask equal to the voxel value for the label
+            region_mask = image_data["data"] == voxel_value
+            # Add the standard mask
+            region_mask = np.logical_and(region_mask, self.standard["mask"])
             voxel_count = np.sum(np.logical_and(region_mask, img).astype(np.int))
             feature_vector.append(int(voxel_count))
         feature_vectors.append(feature_vector)
--- extractRegionalFeatures.py
+++ extractRegionalFeatures.py
@@ -4,13 +4,26 @@
 
 import imageutils as utils
 import glob
+#from visualize import feature_plots
 
 # Create a new Atlas instance to hold our atlas maps
 atlas = utils.Atlas()
 
 # Here our our images to get features for
 files = glob.glob("/home/vanessa/Documents/Work/BRAINBEHAVIOR/mrs/*.nii.gz")
-output_file = "/home/vanessa/Documents/Work/BRAINBEHAVIOR/regional_features.tsv"
+output_file = "/home/vanessa/Documents/Work/BRAINBEHAVIOR/regional_features_Z1pt96.tsv"
 
 # Extract features
-df = atlas.count_voxels_in_atlas(mrs=files,output_file=output_file)
+# specifying the threshold will normalize all images to Z scores, and apply the threshold
+threshold = 1.96
+df = atlas.count_voxels_in_atlas(mrs=files,output_file=output_file,normalize_threshold=threshold)
+
+# Make plots of results
+
+# Resample standard space image to 8x8x8 mm voxel
+standard = "/home/vanessa/Documents/Dropbox/Code/Python/BrainBehavior/brainbehavior/data/standard/MNI152lin_T1_2mm_brain_mask.nii.gz"
+standard8mm = utils.resize_image(standard,[8,8,8])
+
+# Now let's extract a matrix of normalized values
+# IN PROGRESS! :D
+
